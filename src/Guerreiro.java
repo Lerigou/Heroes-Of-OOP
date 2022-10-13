35607e1d53dd4129f7945fb482364fcfeeb12cbb
Lerigou <marciaeduardaravadelli@gmail.com>
2022-10-12 23:43:02 -0300

    Criando o método escolherPersonagem e preenchendo as classes Mago e Arqueiro

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 public class Guerreiro extends Personagem {
-    // Guerreiro - ataque: 30; defesa: 20; ponto de vida: 180
-    // Armas - Espada ataque: +10; defesa: +15
-    // Armas - Machado: ataque: +17; defesa: +8
     private Arma arma;
 
     public Guerreiro(){
@@ -14,26 +11,71 @@ public class Guerreiro extends Personagem {
     @Override
     public void escolherArma() {
         Scanner inputArma = new Scanner(System.in);
-        boolean opcaoInvalido;
+        boolean opcaoInvalida;
 
         do {
-            opcaoInvalido = false;
-            System.out.println("Escolha uma das opções de arma: 1 para espada ou 2 para machado");
+            opcaoInvalida = false;
+            System.out.println("\033[1;97mEscolha uma das opções de arma: 1 para espada ou 2 para machado");
             int armaEscolhida = inputArma.nextInt();
-            if (armaEscolhida == 1) { arma = new Arma(10, 15, "Espada"); }
-            else if (armaEscolhida == 2) { arma = new Arma(17, 8, "Machado"); }
+            if (armaEscolhida == 1) {
+                arma = new Arma(10, 15, "Espada");
+                System.out.println("\n" +
+                        "                   {}\n" +
+                        "                  .--.\n" +
+                        "                 /.--.\\\n" +
+                        "                 |====|\n" +
+                        "                 |`::`|\n" +
+                        "             .-;`\\..../`;_.-^-._\n" +
+                        "      /\\\\   /  |...::..|`   :   `|\n" +
+                        "      |:'\\ |   /'''::''|   .:.   |\n" +
+                        "     @|\\ /\\;-,/\\   ::  |..:::::..|\n" +
+                        "     `||\\ <` >  >._::_.| ':::::' |\n" +
+                        "      || `\"\"`  /   ^^  |   ':'   |\n" +
+                        "      ||       |       \\    :    /\n" +
+                        "      ||       |        \\   :   / \n" +
+                        "      ||       |___/\\___|`-.:.-`\n" +
+                        "      ||        \\_ || _/    `\n" +
+                        "      ||        <_ >< _>\n" +
+                        "      ||        |  ||  |\n" +
+                        "      ||        |  ||  |\n" +
+                        "      ||       _\\.:||:./_\n" +
+                        "      \\/      /____/\\____\\");
+            }
+            else if (armaEscolhida == 2) {
+                arma = new Arma(17, 8, "Machado");
+                System.out.println("\n" +
+                        "   ,   A           {}\n" +
+                        "  / \\, | ,        .--.\n" +
+                        " |    =|= >      /.--.\\\n" +
+                        "  \\ /` | `       |====|\n" +
+                        "   `   |         |`::`|  \n" +
+                        "       |     .-;`\\..../`;_.-^-._\n" +
+                        "      /\\\\/  /  |...::..|`   :   `|\n" +
+                        "      |:'\\ |   /'''::''|   .:.   |\n" +
+                        "       \\ /\\;-,/\\   ::  |..:::::..|\n" +
+                        "       |\\ <` >  >._::_.| ':::::' |\n" +
+                        "       | `\"\"`  /   ^^  |   ':'   |\n" +
+                        "       |       |       \\    :    /\n" +
+                        "       |       |        \\   :   / \n" +
+                        "       |       |___/\\___|`-.:.-`\n" +
+                        "       |        \\_ || _/    `\n" +
+                        "       |        <_ >< _>\n" +
+                        "       |        |  ||  |\n" +
+                        "       |        |  ||  |\n" +
+                        "       |       _\\.:||:./_\n" +
+                        "       |      /____/\\____\\");
+            }
             else {
                 System.out.println("Não existe essa arma");
-                opcaoInvalido = true;
+                opcaoInvalida = true;
             }
-        } while (opcaoInvalido);
+        } while (opcaoInvalida);
 
         setAtaque(arma.getAtaqueArma() + getAtaque());
         setDefesa(arma.getDefesaArma() + getDefesa());
-        System.out.println("Você escolheu a arma " + arma.getNome() + " dando um buff de " + arma.getAtaqueArma()
-                + " no ataque total \ne " + arma.getDefesaArma() + " na defesa total, totalizando em um ataque de "
-                + getAtaque() + " e em uma defesa de " + getDefesa());
-
-
+        System.out.println("\033[1;97mVocê escolheu a arma " + arma.getNome() + "\n" +
+                "\033[1;93m+buff de " + arma.getAtaqueArma()+ " no ataque total \n" +
+                "\033[1;93m+buff de " + arma.getDefesaArma() + " na defesa total \n" +
+                "\033[1;97mTotalizando em um ataque de " + getAtaque() + " e em uma defesa de " + getDefesa() + '\n');
     }
 }
